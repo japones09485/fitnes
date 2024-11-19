b6b05723cebcc8a74dcf84336704e984266e2ade
Jhonatan Leal <65643761+japones09485@users.noreply.github.com>
2024-11-18 23:02:37 -0500

    ok
    20241118

---
+++ application/models/Sedes_gim_model.php
@@ -0,0 +1,13 @@
+<?php
+class Gimnasios_model extends MY_Model {
+	
+	protected $_table = 't_sedes_gimnasio ';
+	function __construct(){
+        // Call the Model constructor
+        parent::__construct();
+	}
+	
+
+
+
+}
